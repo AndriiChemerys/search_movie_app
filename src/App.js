1a0f372d97e2f7d2b940bb80586d480fd8628ef1
AndriiChemerys <andriichemerys@gmail.com>
2022-11-25 15:38:20 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+const Person = () => {
+  return (
+    <>
+      <h1>Name: </h1>
+      <h2>Last Name: </h2>
+      <h2>Age: </h2>
+    </>
+  );
+};
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Person />
     </div>
   );
-}
+};
 
 export default App;
