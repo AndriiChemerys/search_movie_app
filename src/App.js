268911fa6f0a4e589fb3bb49004149d3a4e41a1f
AndriiChemerys <andriichemerys@gmail.com>
2022-11-25 17:56:50 +0100

    Adding Fetch API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,55 @@
 import React from "react";
+import { useEffect } from "react";
+
+import "./App.css";
+import SearchIcon from "./search.svg";
+
+const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=ec97a5e3";
+
+const movie1 = {
+  Title: "Spiderman",
+  Year: "2010",
+  imdbID: "tt1785572",
+  Type: "movie",
+  Poster: "N/A",
+};
 
 const App = () => {
-  return <h1>App</h1>;
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies("Spiderman");
+  }, []);
+
+  return (
+    <div className="app">
+      <h1>MovieLand</h1>
+
+      <div className="search">
+        <input placeholder="Search for movies" value="Superman" onChange={() => {}} />
+        <img src={SearchIcon} alt="search" onClick={() => {}} />
+      </div>
+
+      <div className="container">
+        <div className="movie">
+          <div>
+            <p>{movie1.Year}</p>
+          </div>
+          <div>
+            <img
+              src={movie1.Poster !== "N/A" ? movie1.Poster : "https://via.placeholder.com/400"}
+              alt={movie1.title}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
